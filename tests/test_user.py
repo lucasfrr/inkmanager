a1b846b05f3aefdbc9a181fb6336e5a1e2a19b18
Lucas Ferreira <lucasferreiraek@gmail.com>
2024-11-09 18:11:26 -0300

    feat: add more tests for user with authorization

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from uuid import uuid4
 
 
 def test_create_user(client):
@@ -34,6 +35,23 @@ def test_update_user(client, user, token):
     assert response.json()['email'] == 'toniboy@test.com'
 
 
+def test_update_user_with_wrong_user(client, token):
+    wrong_id = str(uuid4())
+    response = client.put(
+        f'/users/update/{wrong_id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'toniboy',
+            'email': 'toniboy@test.com',
+            'password': 'senha nova',
+            'fullname': 'Toni Boy',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'not enough permissions'}
+
+
 def test_delete_user(client, user, token):
     response = client.delete(
         f'/users/delete/{user.id}',
@@ -42,3 +60,13 @@ def test_delete_user(client, user, token):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'detail': 'user has been deleted'}
+
+
+def test_delete_user_with_wrong_user(client, other_user, token):
+    response = client.delete(
+        f'/users/delete/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'not enough permissions'}
